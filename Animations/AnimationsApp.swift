7a688efd3d9fd51bc3cd675371207292b062386e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-29 15:10:02 -0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Matheus Viana on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
