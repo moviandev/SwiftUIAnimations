b6b833fbf46a84bb735c2491af0cb21ad456bd27
Matheus Viana <matheus_o_viana@hotmail.com>
2023-01-29 16:48:34 -0300

    Animating bindings

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -11,22 +11,25 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap me") {
-            animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .overlay(Circle()
-            .stroke(.red)
-            .scaleEffect(animationAmount)
-            .opacity(2 - animationAmount)
-            .animation(.easeInOut(duration: 1).delay(1).repeatForever(autoreverses: false), value: animationAmount))
-        .onAppear {
-            animationAmount = 2
-        }
+        print(animationAmount)
         
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(3, autoreverses: true)), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            
+        }
     }
 }
 
